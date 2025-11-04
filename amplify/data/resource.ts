953865540fc1080c1a0e2fb7c4ee98a51437b0de
GamesByAlice <gamesbyalice@gmail.com>
2025-11-04 14:23:25 -0800

    Finish quickstart tutorial

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,17 +9,16 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Todo: a
     .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+      content: a.string(),  
+	}).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+  authorizationModes: {    
+    defaultAuthorizationMode: 'userPool',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
