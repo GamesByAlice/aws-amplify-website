f96d0642cbedc4dbac3d782e015ad2f06121b0b0
GamesByAlice <gamesbyalice@gmail.com>
2025-11-22 14:17:32 -0800

    Remove Unused AWS Amplify template

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure(outputs);
 
 try {
   const rootElement = document.getElementById("root");
